5517bb4e814890a94bfcb1645234a48d6c8a4ce7
ibrahimbish1 <s12428512@stu.najah.edu>
2024-11-05 14:54:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-# from firebase_admin import credentials, initialize_app, db
+from firebase_admin import credentials, initialize_app, db
 from datetime import datetime
 
 app = Flask(__name__)
 
-# # Firebase Setup
-# cred = credentials.Certificate(r"C:\Users\user\Desktop\app test4\teth-cbf7b-firebase-adminsdk-6f249-1d34ae36f5.json")
-# initialize_app(cred, {
-#     'databaseURL': 'https://teth-cbf7b-default-rtdb.asia-southeast1.firebasedatabase.app/'
-# })
+# Firebase Setup
+cred = credentials.Certificate(r"C:\Users\user\Desktop\app test4\teth-cbf7b-firebase-adminsdk-6f249-1d34ae36f5.json")
+initialize_app(cred, {
+    'databaseURL': 'https://teth-cbf7b-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
 
 @app.route('/')
 def index():
@@ -25,20 +25,20 @@ def get_names():
     return jsonify(names)
 
 @app.route('/records/<name>')
-# def get_records(name):
-    # records_ref = db.reference(f"/logs/{name}")
-    # records = records_ref.get()
-    # if records:
-    #     sorted_records = sorted(records.values(), key=lambda x: x.get('date', ''))
-    #     records_by_date = {}
-    #     for record in sorted_records:
-    #         date = record.get('date', 'N/A')
-    #         if date not in records_by_date:
-    #             records_by_date[date] = []
-    #         records_by_date[date].append(record)
-    #     return jsonify(records_by_date)
-    # else:
-    #     return jsonify({"message": "No records found."})
+def get_records(name):
+    records_ref = db.reference(f"/logs/{name}")
+    records = records_ref.get()
+    if records:
+        sorted_records = sorted(records.values(), key=lambda x: x.get('date', ''))
+        records_by_date = {}
+        for record in sorted_records:
+            date = record.get('date', 'N/A')
+            if date not in records_by_date:
+                records_by_date[date] = []
+            records_by_date[date].append(record)
+        return jsonify(records_by_date)
+    else:
+        return jsonify({"message": "No records found."})
 
 @app.route('/record_page/<name>')
 def record_page(name):
